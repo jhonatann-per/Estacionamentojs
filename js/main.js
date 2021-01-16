49f621351d493556ad9dfb53128e59cc75497298
jhonatan pereira <jhonatapereira152@gmail.com>
2021-01-15 22:32:41 -0300

    PROJETO TERMINADO.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 document.getElementById('formulario').addEventListener('submit', cadastraVeiculo);
+
 function cadastraVeiculo(e){
 var modeloCarro = document.getElementById('modeloCarro').value;
 var placaCarro = document.getElementById('placaCarro').value;
 var time = new Date();
 
+if(!modeloCarro && !placaCarro){
+    alert("Por Favor, Preencha os campos em branco!")
+    return false;
+}
 carro = {
     modelo: modeloCarro,
     placa: placaCarro,
     hora: time.getHours(),
     minutos: time.getMinutes()
 }
-
+console.log(carro);
 if(localStorage.getItem('patio2') === null) {
     var carros = [];
     carros.push(carro);
@@ -21,9 +26,24 @@ if(localStorage.getItem('patio2') === null) {
     carros.push(carro);
     localStorage.setItem('patio2', JSON.stringify(carros));
 }
+document.getElementById('formulario').reset();
+
+mostraPatio();
 
 e.preventDefault();
 }
+function apagarVeiculo(placa) {
+ var carros = JSON.parse(localStorage.getItem('patio2'));
+ for(var i = 0; i < carros.length; i++){
+     if(carros[i].placa == placa){
+         carros.splice(i, 1);
+     }
+     localStorage.setItem('patio2', JSON.stringify(carros));
+    
+ }
+ mostraPatio();
+}
+
 function mostraPatio(){
    var carros = JSON.parse(localStorage.getItem('patio2'));
    var carrosResultado = document.getElementById('resultados');
@@ -38,6 +58,7 @@ function mostraPatio(){
       carrosResultado.innerHTML += '<tr><td>' + modelo +
        '</td><td>' + placa +
        '</td><td>' + hora + ' : ' + minutos +
-       '</tr>';   
+       '</td><td><button class="btn btn-danger" onclick="apagarVeiculo(\'' + placa + '\')">Excluir</button></td>' +   
+        '</tr>';   
     }
 }
